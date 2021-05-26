093269ca4646928072025d743e0a1e9b4c973f71
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-26 14:28:06 +0530

    Added : Factinality to Check Frist with Regex Parttern

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import com.bridgelabz.com.brigdelabz.validation;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        validation v1 = new validation();
+        System.out.println(v1.FristName());
+        System.out.println(v1.LastName());
+    }
+}
---
+++ src/main/java/com/bridgelabz/com/brigdelabz/validation.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.com.brigdelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class validation {
+    public static String s;
+    public boolean FristName()
+    {
+        System.out.println("enter the first name");
+        Scanner input = new Scanner(System.in);
+        s = input.nextLine();
+        String parttern = "^[A-z]+[a-z]{3,}";
+        Pattern pattern = Pattern.compile(parttern);
+        Matcher matcher = pattern.matcher(s);
+        System.out.println(s);
+        return matcher.find();
+    }
+
+}
