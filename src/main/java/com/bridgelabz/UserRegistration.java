35c707259217dd151417b74ca370d6fef80513d1
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-27 17:20:19 +0530

    Added : Functinality to Accept One Special Charector

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,10 +5,10 @@ import com.bridgelabz.com.brigdelabz.validation;
 public class UserRegistration {
     public static void main(String[] args) {
         validation v1 = new validation();
-//        System.out.println(v1.FristName());
-//        System.out.println(v1.LastName());
-//        System.out.println(v1.email());
-//        System.out.println(v1.mobilel());
+        System.out.println(v1.FristName());
+        System.out.println(v1.LastName());
+        System.out.println(v1.email());
+        System.out.println(v1.mobilel());
         System.out.println(v1.PassWord());
 
     }
